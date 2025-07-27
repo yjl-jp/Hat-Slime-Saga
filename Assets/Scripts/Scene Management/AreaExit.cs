f845d16bfe8270bab12f60de2e5de1e2a3581f4f
yjl-jp <yingjingliang24@gmail.com>
2025-07-27 15:34:17 +0800

    fade transition

--- Assets/Scripts/Scene Management/AreaExit.cs
+++ Assets/Scripts/Scene Management/AreaExit.cs
@@ -8,12 +8,27 @@ public class AreaExit : MonoBehaviour
     [SerializeField] private string sceneToLoad;
     [SerializeField] private string sceneTransitionName;
 
+    private float waitToLoadTime = 1f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         }
     }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        while (waitToLoadTime >= 0)
+        {
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
+
